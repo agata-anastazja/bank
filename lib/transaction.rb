483e7e487c2bc161fced4f003a868233132ad713
Agata Sumowska <agata.sumowska@gmail.com>
2017-01-23 11:59:18 +0000

    Transaction has credit of null by default

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,15 @@
 #Operation on account
 class Transaction
-attr_reader :date
+attr_reader :date, :credit
   def initialize(args = {})
     @date = Date.today.strftime('%m/%d/%Y')
-    
+    @credit = args[:credit] || default_credit
+  end
+
+  private
+
+  def default_credit
+    nil
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -7,7 +7,9 @@ describe Transaction do
     expect(transaction.date).to eq Date.today.strftime('%m/%d/%Y')
   end
 
-
+  it "has credit of null by default" do
+    expect(transaction.credit).to eq nil
+  end
 
 
 
